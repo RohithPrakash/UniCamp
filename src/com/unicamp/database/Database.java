914752825bb6c732ee599d6791c5ed02a063a631
Rohith Prakash <rohithprakash99@gmail.com>
2019-11-25 10:54:43 +0530

    Database connector created

---
+++ src/com/unicamp/database/Database.java
@@ -0,0 +1,30 @@
+package com.unicamp.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+	public static Connection getConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+        String url = "jdbc:mysql://localhost:3306/";
+        String database = "UniCamp";
+        String driver = "com.mysql.jdbc.Driver";
+        String username = "root";
+        String password = "password";
+
+        Class.forName(driver).newInstance();
+        Connection conn = DriverManager.getConnection(url + database, username, password);
+
+        return conn;
+	}
+	
+	public static void closeConnection(Connection conn) {
+        try {
+        	conn.close();
+        } catch(SQLException e) {
+        	System.out.println(e);
+        }
+
+    }
+}
